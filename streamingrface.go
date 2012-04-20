7777f37689a0a928cbe58ac7d71e018f0746d2f2
ryanbressler <ryanbressler@gmail.com>
2012-04-20 12:54:19 -0700

    added in cricvis loading off orzo (internal only)

--- streamingrface.go
+++ streamingrface.go
@@ -160,9 +160,195 @@ func MainServer(w http.ResponseWriter, req *http.Request) {
 	//TODO: stop hardcoding ws url and/or move this html somewhere else
 	io.WriteString(w, `<html>
 <head>
+<title>Wedge Demo</title>
+<link href="http://orzo:8000/assets/css/vq.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="http://orzo:8000/lib/protovis/protovis.js"></script>
+<script type="text/javascript" src="http://orzo:8000/lib/d3/d3.js"></script>
+<script type="text/javascript" src="http://orzo:8000/lib/d3/d3.layout.js"></script>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://orzo:8000/lib/underscore/underscore.js"></script>
+<script type="text/javascript" src="http://orzo:8000/vq.js"></script>
+<script type="text/javascript" src="http://orzo:8000/vq.wedge.js"></script>
+
+<script type="text/javascript" src="http://orzo:8000/data/GBM_cnv_track.json"></script>
+<script type="text/javascript" src="http://orzo:8000/data/GBM_link_density_track.json"></script>
+<script type="text/javascript" src="http://orzo:8000/data/GBM_tick_track.json"></script>
+<script type="text/javascript" src="http://orzo:8000/data/GBM_network.json"></script>
+<script type="text/javascript" src="http://orzo:8000/data/chromInfo.json"></script>
+<script type="text/javascript" src="http://orzo:8000/data/cytoband.hg18.json"></script>
+
+
+<body onLoad="init();">
+<div id="wedge" style="font: 10px sans-serif;margin:10px"></div>
+
+<form name="msgform" action="#" onsubmit="return send();">
+A json array of ints coresponding to feature indexes:<input type="text" name="message" size="80" value="[0,1,2,3,4,5,6,18162,0,30798,30797,4986,4987,4988,4989]">
+<input type="submit" value="send">
+</form>
+<div id="logdiv"></div>
 <script type="text/javascript">
 var path;
 var ws;
+var circle;
+
+
+
+function wedge_plot(div) {
+    var width=1200, height=1200;
+    var	cnv_ring_height = 60;
+
+      var tick_keys = ['METH','CNV','hsa-miR','Other'],
+        color_scale = d3.scale.category10().domain(tick_keys);
+
+    var tick_colors = function(data) {
+           var tick_key = 'Other';
+           tick_keys.forEach(function(key){
+           if (data.label.indexOf(key) == 0)  //starts with key
+           tick_key = key;
+           });
+       return color_scale(tick_key);
+       };
+
+
+    var data = {
+   PLOT: {
+            container: div,
+            width : width,
+            height: height,
+            vertical_padding : 10,
+            horizontal_padding: 10,
+            enable_pan : false,
+            enable_zoom : false,
+            show_legend: true,
+            legend_corner : 'ne',
+            legend_radius  : 45
+        },
+
+        GENOME: {
+            DATA:{
+                key_order : chrom_keys,
+                key_length : chrom_leng
+            },
+            OPTIONS: {
+                radial_grid_line_width : 2,
+                label_layout_style:'clock',
+                label_font_style:'16px helvetica'
+           }
+        },
+
+        WEDGE:[
+                 {
+               PLOT : {
+                height : 20,
+                   type : 'karyotype'
+            } ,
+                DATA:{
+                    data_array : cytoband
+                },
+                OPTIONS: {
+
+                    legend_label : 'Karyotype Bands',
+                    legend_description : 'Karyotype Bands',
+                    listener : function() {return null;},
+                    outer_padding: 15
+                }
+            },
+            {   PLOT : {
+                height : cnv_ring_height,
+                   type : 'histogram'
+            } ,
+                DATA:{
+                    data_array : []//cnv
+                },
+                OPTIONS: {
+                    base_value : 0,
+                    legend_label : 'Mean CNV',
+                    legend_description : 'Mean Copy Number Variation',
+                    listener : listener,
+                    outer_padding: 15
+                }
+            },
+            {
+                PLOT : {
+
+                    height : 60,
+                    type : 'scatterplot'
+                },
+                DATA:{
+                    data_array : [],//link_density,
+                    value_key : 'value'
+                },
+                OPTIONS: {
+                    legend_label : 'Link Density',
+                    legend_description : 'Density of Inferred Links',
+                    min_value : 0,
+                    max_value : 6,
+                    base_value : 0,
+                    radius : 3,
+                    shape : 'circle',
+                    outer_padding: 15,
+                    stroke_style : null,
+                    listener : listener
+                }
+            }
+        ],
+
+        TICKS : {
+           DATA: {
+                data_array:[]// ticks.map(function(tick) { return vq.utils.VisUtils.extend({label:tick.value},tick);})
+            },
+            OPTIONS : {
+                height : 120,
+                display_legend : true,
+                legend_corner : 'nw',
+                fill_style : tick_colors,
+                label_map : [{key:{label:'METH'}, label: 'Methylation'},
+                            {key:{label:'CNV'} , label: 'Copy Number Variation'},
+                            {key:{label:'hsa-miR'}, label:'uRNA'},
+                            {key:{label:'Other'}, label: 'Gene Expression'}]
+
+            }
+        },
+
+        NETWORK:{
+            DATA:{
+                data_array : []//
+            },
+            OPTIONS: {
+                outer_padding : 10,
+                node_highlight_mode : 'isolate',
+                node_fill_style : 'steelblue',
+                link_stroke_style : 'red',
+                link_alpha : 0.3,
+                node_key : function(node) { return vq.utils.VisUtils.options_map(node)['label'];},
+                node_tooltip_items :  {
+                    Chr : 'chr',
+                    Start : 'start',
+                    End : 'end',
+                    Label : function(c) { return vq.utils.VisUtils.options_map(c)['label']; }
+                },
+                link_tooltip_items :  {
+                    'Node 1 Chr' : 'sourceNode.chr', 'Node 1 Start' : 'sourceNode.start', 'Node 1 End' : 'sourceNode.end',
+                    'Node 1 Label' : function(c) { return vq.utils.VisUtils.options_map(c.sourceNode)['label'];},
+                    'Node 2 Chr' : 'targetNode.chr', 'Node 2 Start' : 'targetNode.start', 'Node 2 End' : 'targetNode.end',
+                    'Node 2 Label' : function(c) { return vq.utils.VisUtils.options_map(c.targetNode)['label'];}
+		}
+            }
+        }
+    };
+
+    var listener = function(list) { return console.log("listener!"); };
+    var circle_vis = new vq.CircVis();
+    var dataObject ={DATATYPE : "vq.models.CircVisData", CONTENTS : data };
+
+
+    circle_vis.draw(dataObject);
+
+    return circle_vis;
+}
+
+circle = wedge_plot(document.getElementById('wedge'));
 
 function log(msg){
 	console.log(msg);
@@ -184,8 +370,16 @@ function init() {
       log("opened\n");
    };
    ws.onmessage = function (e) {
+   	log("got:" + e.data);
+   	var edge_json = JSON.parse(e.data);
+   	var edges = _.filter(edge_json,function(edge) { return edge.node1.chr != "" && edge.node2.chr != "";});
+   	if (edges.length > 0) {
+  	   	_.each(edges,function(edge){ edge.node1.chr = edge.node1.chr.slice(3); edge.node2.chr = edge.node2.chr.slice(3);});
+  	   	console.log(JSON.stringify(edges));
+   	circle.addEdges(edges);
+   }
    	  
-      log("got:" + e.data);
+      
    };
    ws.onclose = function (e) {
      log("closed\n");
@@ -202,12 +396,6 @@ function send() {
    return false;
 };
 </script>
-<body onLoad="init();">
-<form name="msgform" action="#" onsubmit="return send();">
-A json array of ints coresponding to feature indexes:<input type="text" name="message" size="80" value="[0,1,2,3,4,5,6,18162,0,30798,30797,4986,4987,4988,4989]">
-<input type="submit" value="send">
-</form>
-<div id="logdiv"></div>
 </html>
 `)
 }
